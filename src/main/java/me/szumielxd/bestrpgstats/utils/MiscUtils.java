795e2d958b601d740db01af2cbeaf00ff61db689
szumielxd <43210079+szumielxd@users.noreply.github.com>
2022-08-22 09:47:05 +0200

    Added vanish check

--- src/main/java/me/szumielxd/bestrpgstats/utils/MiscUtils.java
+++ src/main/java/me/szumielxd/bestrpgstats/utils/MiscUtils.java
@@ -4,17 +4,23 @@ import java.util.Objects;
 import java.util.Optional;
 import java.util.regex.Pattern;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.metadata.MetadataValue;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.kitteh.vanish.VanishPlugin;
+import com.earth2me.essentials.Essentials;
+import com.earth2me.essentials.User;
 
 import ch.njol.skript.variables.Variables;
+import lombok.experimental.UtilityClass;
 import me.clip.placeholderapi.PlaceholderAPI;
 import me.szumielxd.bestrpgstats.BestrpgStats;
 import me.szumielxd.bestrpgstats.ConfigKey;
 
+@UtilityClass
 public class MiscUtils {
 	
 	
@@ -43,13 +49,21 @@ public class MiscUtils {
 			Object obj = Variables.getVariable(accessor.replace("%player%", player.getName()), null, false);
 			if (obj == null) return def;
 			if (obj instanceof Number) return ((Number) obj).longValue();
-			if (obj instanceof String) try { return Long.parseLong((String) obj); } catch (NumberFormatException e) {}
+			if (obj instanceof String) try {
+				return Long.parseLong((String) obj);
+			} catch (NumberFormatException e) {
+				// skipping
+			}
 			return def;
 		} else if ("placeholderapi".equalsIgnoreCase(type)) {
 			accessor = accessor.replace("%player%", player.getName());
-			final String acc = accessor;
-			return Optional.of(PlaceholderAPI.setPlaceholders(player, accessor)).filter(str -> !acc.equals(str)).map(str -> {
-				try { Long.parseLong(str); } catch (NumberFormatException e) {} return (Long) null;
+			final String acc = "%"+accessor+"%";
+			return Optional.of(PlaceholderAPI.setPlaceholders(player, acc)).filter(str -> !acc.equals(str)).map(str -> {
+				try {
+					return Long.valueOf(str);
+				} catch (NumberFormatException e) {
+					return null;
+				}
 			}).orElse(def);
 		}
 		return def;
@@ -74,8 +88,8 @@ public class MiscUtils {
 			return obj.toString();
 		} else if ("placeholderapi".equalsIgnoreCase(type)) {
 			accessor = accessor.replace("%player%", player.getName());
-			final String acc = accessor;
-			return Optional.of(PlaceholderAPI.setPlaceholders(player, accessor)).filter(str -> acc.equalsIgnoreCase(str)).orElse(def);
+			final String acc = "%"+accessor+"%";
+			return Optional.of(PlaceholderAPI.setPlaceholders(player, acc)).filter(acc::equalsIgnoreCase).orElse(def);
 		}
 		return def;
 	}
@@ -96,13 +110,21 @@ public class MiscUtils {
 			Object obj = Variables.getVariable(accessor.replace("%player%", player.getName()), null, false);
 			if (obj == null) return def;
 			if (obj instanceof Number) return ((Number) obj).intValue();
-			if (obj instanceof String) try { return Integer.parseInt((String) obj); } catch (NumberFormatException e) {}
+			if (obj instanceof String) try {
+				return Integer.parseInt((String) obj);
+			} catch (NumberFormatException e) {
+				// skipping
+			}
 			return def;
 		} else if ("placeholderapi".equalsIgnoreCase(type)) {
 			accessor = accessor.replace("%player%", player.getName());
-			final String acc = accessor;
-			return Optional.of(PlaceholderAPI.setPlaceholders(player, accessor)).filter(str -> !acc.equals(str)).map(str -> {
-				try { Integer.parseInt(str); } catch (NumberFormatException e) {} return (Integer) null;
+			final String acc = "%"+accessor+"%";
+			return Optional.of(PlaceholderAPI.setPlaceholders(player, acc)).filter(str -> !acc.equals(str)).map(str -> {
+				try { 
+					Integer.valueOf(str);
+				} catch (NumberFormatException e) {
+					// defaulting to def
+				} return def;
 			}).orElse(def);
 		}
 		return def;
@@ -114,6 +136,32 @@ public class MiscUtils {
 		if (!item.hasItemMeta()) return false;
 		if (!item.getItemMeta().hasDisplayName()) return false;
 		return TALISMAN_PATTERN.matcher(item.getItemMeta().getDisplayName()).matches();
+	}
+	
+	
+	public static boolean isVanished(@NotNull Player player) {
+		Objects.requireNonNull("player cannot be null");
+		try {
+			Class.forName("com.earth2me.essentials.Essentials");
+			Essentials ess = (Essentials) Bukkit.getPluginManager().getPlugin("Essentials");
+			User user = ess.getUser(player);
+			if (user != null && user.isVanished()) return true;
+		} catch (ClassNotFoundException e) {
+			// Essentials not found
+		}
+		try {
+			Class.forName("org.kitteh.vanish.VanishPlugin");
+			VanishPlugin vnp = (VanishPlugin) Bukkit.getPluginManager().getPlugin("VanishNoPacket");
+			if (vnp.getManager().isVanished(player)) return true;
+		} catch (ClassNotFoundException e) {
+			// VanishNoPacket not found
+		}
+		return false;
+	}
+	
+	
+	public static boolean isNotVanished(@NotNull Player player) {
+		return !isVanished(player);
 	}
 	
 
